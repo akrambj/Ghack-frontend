c219a72d13d36b9108eddc5c63c07b5488393415
akrambj <bedjaouiakram.93@gmail.com>
2024-02-22 19:24:57 +0000

    json-server

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,17 @@
+import { useEffect } from "react";
+
 const Home = () => {
+  useEffect(() => {
+    const getData = async () => {
+      const res = await fetch("http://localhost:9000/users");
+      console.log(res, "res");
+      const data = await res.data;
+      console.log(data, "data");
+    };
+
+    getData();
+  }, []);
+
   return <div>Home</div>;
 };
 
