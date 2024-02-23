05f6fc69b27cc9069629bdd2022b862efea5d3a7
akrambj <bedjaouiakram.93@gmail.com>
2024-02-23 12:56:41 +0000

    manager footer

--- src/components/manager/shared/footer/ManagerFooter.jsx
+++ src/components/manager/shared/footer/ManagerFooter.jsx
@@ -1,5 +1,12 @@
 const ManagerFooter = () => {
-  return <div>footer</div>;
+  return (
+    <footer className="w-screen bg-white pt-4 flex flex-col gap-4">
+      <div className="bg-[#E7EDF6] w-full flex justify-between px-20 items-center py-3">
+        <h5>Privacy Policy</h5>
+        <h5>Doctym ©2023, All right reserved.</h5>
+      </div>
+    </footer>
+  );
 };
 
 export default ManagerFooter;
