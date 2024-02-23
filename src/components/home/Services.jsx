c1ba5cb6089d9d3782df903c76bf4ee20f960237
akrambj <bedjaouiakram.93@gmail.com>
2024-02-23 00:16:11 +0000

    services

---
+++ src/components/home/Services.jsx
@@ -0,0 +1,32 @@
+import Workspace from "../services/Workspace";
+
+const Services = () => {
+  return (
+    <section className="w-full h-screen overflow-hidden flex flex-col justify-center items-center">
+      <div className=" flex items-center justify-center p-10 gap-10 flex-wrap bg-[#F3FAFF] drop-shadow-md shadow-sm w-[70%] mx-auto">
+        <Workspace
+          bg="#66DC90"
+          title={"Personal Workspaces"}
+          text={"+40k users"}
+        />
+        <Workspace
+          bg={"#BFB7F1"}
+          title={"Intelligent Cordinator"}
+          text={"+50k users"}
+        />
+        <Workspace
+          bg={"#56A0EA"}
+          title={"Manager's Decision panel"}
+          text={"+60k users"}
+        />
+        <Workspace
+          bg={"#0AE7EF"}
+          title={"Virtual Collaboration Room"}
+          text={"+50k users"}
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Services;
